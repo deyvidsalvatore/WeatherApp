2c332556b0bd17b38fc40d48cc49c97582b74640
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-27 11:45:57 -0300

    feat: Search Component and Fixing Memory Leaks

--- src/app/weather/pages/weather-home/weather-home.component.ts
+++ src/app/weather/pages/weather-home/weather-home.component.ts
@@ -1,28 +1,49 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { WeatherData } from './../../shared/interfaces/weather-data.interface';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
+import { Subject, takeUntil } from 'rxjs';
 
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch'
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-weather-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, FontAwesomeModule],
   templateUrl: './weather-home.component.html',
   styleUrl: './weather-home.component.scss',
   providers: [WeatherService]
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
 
+  private readonly destroy$: Subject<void> = new Subject();
   private _weatherService: WeatherService = inject(WeatherService);
 
-  initialName: string = "Brasilia";
+  public readonly searchIcon = faSearch;
+
+  initialCityName: string = "Brasilia";
 
   weatherData?: WeatherData;
+
   ngOnInit(): void {
-    this.getWeatherDatas(this.initialName);
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  ngOnDestroy(): void {
+    this.initialCityName = '';
+
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
   }
 
   getWeatherDatas(cityName: string): void {
     this._weatherService.getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (response) => {
           response && (this.weatherData = response);
